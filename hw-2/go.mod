cbb82249691fc449db036c5f43af1d716009e7ad
Mark Bykhovets <mb@hrsmnt.ru>
2020-07-28 23:44:16 +0300

    unpack function done

---
+++ hw-2/go.mod
@@ -0,0 +1,3 @@
+module github.com/mark-by/golang/hw-2
+
+go 1.14
---
+++ hw-2/unpackStr.go
@@ -0,0 +1,38 @@
+package unpack
+
+import (
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+//Unpack string. "a4bc2d5e" => "aaaabccddddde" and `qwe\4\5` => `qwe45`
+func Unpack(str string) string {
+	var builder strings.Builder
+	reader := strings.NewReader(str)
+	prevChar, _, _ := reader.ReadRune()
+	if unicode.IsDigit(prevChar) {
+		return ""
+	}
+	for {
+		currChar, _, readErr := reader.ReadRune()
+		if readErr != nil {
+			builder.WriteRune(prevChar)
+			break
+		}
+
+		digit, atoiErr := strconv.Atoi(string(currChar))
+		if atoiErr == nil {
+			builder.WriteString(strings.Repeat(string(prevChar), digit))
+		} else {
+			builder.WriteRune(prevChar)
+		}
+
+		if currChar == '\\' || atoiErr == nil {
+			prevChar, _, _ = reader.ReadRune()
+		} else {
+			prevChar = currChar
+		}
+	}
+	return builder.String()
+}
